c8108005bda53d88996819c1f7424309fd824846
Jacob Phillips <jacob.phillips8905@gmail.com>
2025-07-31 13:02:38 -0700

    updating train on rev text

--- verifiers/examples/reverse_text.py
+++ verifiers/examples/reverse_text.py
@@ -14,7 +14,6 @@ model_name = 'willcb/Qwen3-4B'
 dataset = load_dataset('agentlans/wikipedia-paragraphs', split='train').map(lambda x: {'question': x['text'], 'answer': x['text'][::-1]})
 num_train = 9500
 num_eval = 500
-
 train_dataset = dataset.select(range(num_train)) # type: ignore
 eval_dataset = dataset.select(range(num_train, num_train + num_eval)) # type: ignore
 parser = vf.XMLParser(['think', 'answer'], answer_field='answer')
@@ -23,23 +22,18 @@ system_prompt = f"""Reverse the given text.
 Respond in the following format:
 {parser.get_format_str()}"""
 
-def lcs_ratio(x: str, y: str) -> float:
-    """
-    Return the longest common subsequence ratio of x and y.
-    """
-    from difflib import SequenceMatcher
-    return SequenceMatcher(None, x, y).ratio()
-
 def lcs_reward_func(completion, answer, **kwargs) -> float:
     """
     LCS ratio of the reversed prompt and the parsed completion.
     """
+    def lcs_ratio(x: str, y: str) -> float:
+        """
+        Return the longest common subsequence ratio of x and y.
+        """
+        from difflib import SequenceMatcher
+        return SequenceMatcher(None, x, y).ratio()
     response = parser.parse_answer(completion) or ''
     return lcs_ratio(response, answer)
-
-
-
-
 
 rubric = vf.Rubric(funcs=[
 	lcs_reward_func,
