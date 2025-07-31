b7cf16958052de9a2c643eeb7b961474e5f4dc1b
Jacob Phillips <jacob.phillips8905@gmail.com>
2025-07-31 13:14:31 -0700

    for train 4b

--- verifiers/examples/reverse_text.py
+++ verifiers/examples/reverse_text.py
@@ -50,7 +50,7 @@ vf_env = vf.SingleTurnEnv(
 args = vf.grpo_defaults(run_name='reverse_text_warmup')
 args.per_device_train_batch_size = 12
 args.num_generations = 12
-args.gradient_accumulation_steps = 8
+args.gradient_accumulation_steps = 4
 args.max_steps = 100
 args.eval_strategy = 'steps'
 args.eval_steps = 2
